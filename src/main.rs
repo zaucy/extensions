8182065b5960e2681876be747a58e34cbccb5527
Marshall Bowers <elliott.codes@gmail.com>
2024-02-27 23:32:51 -0500

    Get theme packaging working

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::collections::HashMap;
 use std::fmt;
 use std::path::{Path, PathBuf};
 
-use anyhow::Result;
+use anyhow::{bail, Result};
 use derive_more::Deref;
 use indexmap::IndexMap;
 use serde::de::DeserializeOwned;
@@ -75,10 +75,146 @@ async fn package_extension(
     extension_version: &str,
     should_publish: bool,
 ) -> Result<()> {
+    let (metadata, _format) = read_extension_manifest(&extension_path).await?;
+
+    if metadata.version != extension_version {
+        let error = [
+            format!(
+                "Incorrect version for extension {extension_id} ({name})",
+                name = metadata.name
+            ),
+            "".to_string(),
+            format!("Expected version: {extension_version}"),
+            format!("Actual version: {}", metadata.version),
+        ]
+        .join("\n");
+
+        bail!(error)
+    }
+
+    let mut package_manifest = ExtensionManifest {
+        name: metadata.name.clone(),
+        version: metadata.version,
+        description: metadata.description,
+        repository: metadata.repository,
+        authors: metadata.authors,
+        lib: None,
+        themes: Vec::new(),
+        languages: Vec::new(),
+        grammars: IndexMap::new(),
+        language_servers: IndexMap::new(),
+    };
+
+    let package_dir = tempfile::tempdir_in("build")?;
+    let archive_name = package_dir
+        .path()
+        .join(format!("{extension_id}-{}", package_manifest.version))
+        .with_extension("tar.gz");
+
+    // let mut grammar_repo_paths = HashMap::new();
+
+    let grammars_src_dir = extension_path.join("grammars");
+    let languages_src_dir = extension_path.join("languages");
+    let themes_src_dir = extension_path.join("themes");
+
+    let grammars_pkg_dir = package_dir.path().join("grammars");
+    let languages_pkg_dir = package_dir.path().join("languages");
+    let themes_pkg_dir = package_dir.path().join("themes");
+
+    if is_directory(&themes_src_dir).await {
+        fs::create_dir(&themes_pkg_dir).await?;
+
+        let mut read_dir = fs::read_dir(themes_src_dir).await?;
+        while let Some(theme_entry) = read_dir.next_entry().await? {
+            let Some(theme_filename) = theme_entry
+                .file_name()
+                .to_str()
+                .map(|name| name.to_string())
+            else {
+                continue;
+            };
+
+            let theme: serde_json::Value = read_json_file(&theme_entry.path()).await?;
+
+            validate_theme(&theme)?;
+
+            let theme_destination_path = themes_pkg_dir.join(&theme_filename);
+            fs::copy(theme_entry.path(), theme_destination_path).await?;
+            package_manifest
+                .themes
+                .push(PathBuf::from_iter(["themes", &theme_filename]));
+        }
+    }
+
     Ok(())
 }
 
-async fn read_extension_manifest(extension_path: &str) {}
+async fn is_directory(path: impl AsRef<Path>) -> bool {
+    match fs::metadata(path).await {
+        Ok(metadata) => metadata.is_dir(),
+        Err(_) => false,
+    }
+}
+
+#[derive(Clone, PartialEq, Eq, Debug, Deserialize)]
+pub struct ExtensionManifest {
+    // pub id: String,
+    pub name: String,
+    pub version: String,
+    #[serde(default)]
+    pub description: Option<String>,
+    #[serde(default)]
+    pub repository: Option<String>,
+    #[serde(default)]
+    pub authors: Vec<String>,
+    #[serde(default)]
+    pub lib: Option<LibManifestEntry>,
+    #[serde(default)]
+    pub themes: Vec<PathBuf>,
+    #[serde(default)]
+    pub languages: Vec<PathBuf>,
+    #[serde(default)]
+    pub grammars: IndexMap<String, GrammarManifestEntry>,
+    #[serde(default)]
+    pub language_servers: IndexMap<String, LanguageServerManifestEntry>,
+}
+
+#[derive(Clone, Default, PartialEq, Eq, Debug, Deserialize)]
+pub struct LibManifestEntry {
+    path: String,
+}
+
+#[derive(Clone, Default, PartialEq, Eq, Debug, Deserialize)]
+pub struct GrammarManifestEntry {
+    repository: String,
+    #[serde(alias = "commit")]
+    rev: String,
+}
+
+#[derive(Clone, PartialEq, Eq, Debug, Deserialize)]
+pub struct LanguageServerManifestEntry {
+    name: String,
+    language: String,
+}
+
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Hash, Clone, Copy)]
+enum ExtensionManifestFormat {
+    Toml,
+    Json,
+}
+
+async fn read_extension_manifest(
+    extension_path: &Path,
+) -> Result<(ExtensionManifest, ExtensionManifestFormat)> {
+    if let Some(manifest) =
+        try_read_toml_file(extension_path.to_path_buf().join("extension.toml")).await?
+    {
+        return Ok((manifest, ExtensionManifestFormat::Toml));
+    }
+
+    let manifest = read_json_file(extension_path.to_path_buf().join("extension.json")).await?;
+    Ok((manifest, ExtensionManifestFormat::Json))
+}
 
 async fn read_json_file<T: DeserializeOwned>(path: impl AsRef<Path>) -> Result<T> {
     let mut json_file = File::open(path).await?;
@@ -89,6 +225,24 @@ async fn read_json_file<T: DeserializeOwned>(path: impl AsRef<Path>) -> Result<T
     Ok(serde_json_lenient::from_str(&buffer)?)
 }
 
+async fn try_read_toml_file<T: DeserializeOwned>(path: impl AsRef<Path>) -> Result<Option<T>> {
+    let mut toml_file = match File::open(path).await {
+        Ok(file) => file,
+        Err(err) => {
+            if err.kind() == std::io::ErrorKind::NotFound {
+                return Ok(None);
+            }
+
+            Err(err)?
+        }
+    };
+
+    let mut buffer = String::new();
+    toml_file.read_to_string(&mut buffer).await?;
+
+    Ok(toml::from_str(&buffer)?)
+}
+
 async fn read_toml_file<T: DeserializeOwned>(path: impl AsRef<Path>) -> Result<T> {
     let mut toml_file = File::open(path).await?;
 
@@ -96,6 +250,12 @@ async fn read_toml_file<T: DeserializeOwned>(path: impl AsRef<Path>) -> Result<T
     toml_file.read_to_string(&mut buffer).await?;
 
     Ok(toml::from_str(&buffer)?)
+}
+
+fn validate_theme(theme: &serde_json::Value) -> Result<()> {
+    // TODO: Validate theme.
+
+    Ok(())
 }
 
 async fn get_published_versions_by_extension_id() -> Result<HashMap<ExtensionId, Vec<String>>> {
